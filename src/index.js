29bbab702ff7b2339e8edcf7e1e5c8bb66e8c628
Robert imbrie <robert.m.imbrie@gmail.com>
2021-02-24 13:25:04 -0600

    Turn Square into function component
    **Notes:**
    Suppose a component only has a render() method and doesn't actual contain a state. There's an easier way to write this: a function component.
    - All you need is a single method: a return call which contains a JSX component.
    - Functionally identical, but a heck of a lot less boilerplate.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button
-            className="square"
-            onClick={() =>this.props.onClick()}
-        >
-          {this.props.value}
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick()}>
+          {props.value}
         </button>
       );
     }
