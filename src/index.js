dcc788cac3adade65d80e74e6a3c7e55eb441867
Robert imbrie <robert.m.imbrie@gmail.com>
2021-03-02 13:20:47 -0500

    Render board by loop
    - Replace hand-coded board with (janky) loop

--- src/index.js
+++ src/index.js
@@ -21,23 +21,22 @@ function Square(props) {
     }
   
     render() {
+      const boardRows = [];
+      for(var i=0; i<3; i++) {
+        const rowStart = i*3;
+        const rowSquares = [];
+        for(var j=rowStart; j<rowStart+3; j++) {
+          rowSquares.push(this.renderSquare(j));
+        }
+        boardRows.push(
+          <div className="board-row">
+            {rowSquares}
+          </div>
+        );
+      }
       return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+          {boardRows}
         </div>
       );
     }
